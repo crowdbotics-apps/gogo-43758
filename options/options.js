5ad38bfeec161c30bb58f9f2a9cc2f1f104d5a77
Crowdbotics <team@crowdbotics.com>
2023-10-04 18:35:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gogo-43758",
+  url: "https://gogo-43758.botics.co",
+  api: "https://gogo-43758.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
